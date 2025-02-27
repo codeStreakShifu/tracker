e2ee71e4cc19aa3c9779170864825606c40d7fe0
codeStreakShifu <undertaker0000aw@gmail.com>
2025-02-27 22:08:42 +0800

    Update db.php

--- db.php
+++ db.php
@@ -6,7 +6,7 @@ date_default_timezone_set('Asia/Manila');
 $host = 'localhost';
 $db   = 'attendance_tracker'; // your database name
 $user = 'root';               // your database username
-$pass = '';              // your database password
+$pass = 'admin';              // your database password
 $charset = 'utf8mb4';
 $port = 3306;                 // your MySQL server port
 
